72674762660fea2b719cd48e26330dcf3bb0d1cc
brianaabreu <briana.abreu01@gmail.com>
2021-09-30 19:31:24 -0700

    url

--- src/api/index.js
+++ src/api/index.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
-const url = 'https://final-writingroom.herokuapp.com/posts';
+const url = 'https://final-writingroom.herokuapp.com';
 
-export const fetchPosts = () => axios.get(url);
-export const createPost = (newPost) => axios.post(url, newPost)
-export const likePost = (id) => axios.patch(`${url}/${id}/likePost`);
-export const updatePost = (id, updatedPost) => axios.patch(`${url}/${id}`, updatedPost);
-export const deletePost = (id) => axios.delete(`${url}/${id}`); 
+export const fetchPosts = () => axios.get(`${url}/posts`);
+export const createPost = (newPost) => axios.post(`${url}/posts`, newPost)
+export const likePost = (id) => axios.patch(`${url}/posts/${id}/likePost`);
+export const updatePost = (id, updatedPost) => axios.patch(`${url}/posts/${id}`, updatedPost);
+export const deletePost = (id) => axios.delete(`${url}/posts/${id}`); 
