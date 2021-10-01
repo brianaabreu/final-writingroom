2e21cbca51af6476c8466449d2d6bf465437ccff
brianaabreu <briana.abreu01@gmail.com>
2021-09-30 22:35:31 -0700

    fixed files

--- server/index.js
+++ server/index.js
@@ -12,6 +12,10 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
+app.get('/', (req, res) => {
+    res.send('HELLO!!')
+})
+
 app.use('/posts', postRoutes)
 const CONNECTION_URL = 'mongodb+srv://brianaabreu:3439591b@cluster0.yhzp7.mongodb.net/<dbname>?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000;
